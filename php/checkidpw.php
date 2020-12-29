baa26890d2223ae8b55cd6941f17d42e616e8ced
Tatsumiking <tatsumiking@gmail.com>
2020-12-29 09:46:30 +0900

    jsbridal ver1.0.0.0 2020/12/29 10:00

--- php/checkidpw.php
+++ php/checkidpw.php
@@ -1,7 +1,5 @@
 <?php
-
 $argvs = $_POST['com'];
-
 //$argvs = "012950,429866";
 $ary = explode(",", $argvs);
 $strID = $ary[0];
@@ -14,22 +12,19 @@ if($strID == $sID && $strPW == $sPW){
 	echo "1,1,";
 	return;
 }
-
-$sIdxList = "";
-$sIdxList = $sIdxList . "0,1,3,7,8";
-$sIdxList = $sIdxList . ";1,2,6,8,9";
-$sIdxList = $sIdxList . ";2,4,5,7,8";
-$sIdxList = $sIdxList . ";1,3,4,6,9";
-$sIdxList = $sIdxList . ";0,2,3,7,8";
-$sIdxList = $sIdxList . ";0,1,4,5,6";
-$sIdxList = $sIdxList . ";2,3,5,6,7";
-$sIdxList = $sIdxList . ";0,1,2,5,6";
-$sIdxList = $sIdxList . ";0,3,6,7,9";
-$sIdxList = $sIdxList . ";1,2,4,5,9";
-
-$m_aryIdx = explode(";", $sIdxList);
-$sPWList = "9,5,7,2,6,4,3,8,0,1";
-$m_aryPW = explode(",", $sPWList);
+$m_aryIdx = array(
+	array(0,1,3,7,8),
+	array(1,2,6,8,9),
+	array(2,4,5,7,8),
+	array(1,3,4,6,9),
+	array(0,2,3,7,8),
+	array(0,1,4,5,6),
+	array(2,3,5,6,7),
+	array(0,1,2,5,6),
+	array(0,3,6,7,9),
+	array(1,2,4,5,9)
+);
+$m_aryPW = array("9","5","7","2","6","4","3","8","0","1");
 
 $klen = strlen($m_csKey);
 $sPW = "";
@@ -38,9 +33,7 @@ $pid = 0;
 for($idx = 0; $idx < $max; $idx++){
 	$num = 0;
 	for($aidx = 0; $aidx < 5; $aidx++){
-		$sIdx = $m_aryIdx[$idx];
-		$ary = explode(",", $sIdx);
-		$tidx = $ary[$aidx];
+		$tidx = $m_aryIdx[$idx][$aidx];
 		if($tidx < $max){
 			$ch = substr($strID, $tidx, 1);
 			$num = $num + ord($ch);
@@ -53,9 +46,8 @@ for($idx = 0; $idx < $max; $idx++){
 	$sPW = $sPW.$m_aryPW[$tidx];
 }
 if($strPW == $sPW){
-	echo "1,0,".$strPW."=".$sPW;
+	echo "1,0,";
 }else{
-	echo "0,0,".$strPW."=".$sPW;	
+	echo "0,0,";	
 }
-
 ?> 
